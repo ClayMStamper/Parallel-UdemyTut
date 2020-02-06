26be3442c8f4a47b40f41eb6d39464e7df5b90e7
clayton <claystamper@gmail.com>
2020-02-05 18:05:25 -0600

    added bank account class with locked access to bank balance

--- ThreadingTut/Program.cs
+++ ThreadingTut/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,9 +7,60 @@ namespace ThreadingTut {
     class Program {
 
 
+        public class BankAccount {
+            
+            public object padlock = new object();
+
+            private int balance;
+
+            public int Balance {
+                get {
+                    return balance;
+                }
+                private set {
+                    balance = value;
+                }
+            }
+
+            public void Deposit(int amount) {
+
+                Interlocked.Add(ref balance, amount);
+
+//                lock (padlock) {
+//                    Balance = Balance + amount;
+//                }
+            }
+
+            public void Withdraw(int amount) {
+                Interlocked.Add(ref balance, -amount);
+            }
+            
+            
+        }
+        
         static void Main(string[] args) {
 
-            OnFinishedMain();
+            var tasks = new List<Task>();
+            var ba = new BankAccount();
+
+            for (int i = 0; i < 10; i++) {
+                tasks.Add(Task.Factory.StartNew(() => {
+                    for (int j = 0; j < 1000; j++) {
+                        ba.Deposit(100);
+                    }
+                }));
+                
+                tasks.Add(Task.Factory.StartNew(() => {
+                    for (int j = 0; j < 1000; j++) {
+                        ba.Withdraw(100);
+                    }
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            
+            Console.WriteLine(ba.Balance);
+            
             
         }
         
